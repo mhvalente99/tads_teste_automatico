65f8c7afc3ba58f18969014afa2935ab9d4ded1a
Matheus Henrique <matheushenrique@MacBook-Pro-de-Matheus.local>
2018-07-30 22:26:05 -0300

    Corrigindo testes de divisao por zero

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,14 +1,24 @@
 
 public class Calculadora {
 
-	public Double calcular(Double n1, Double n2, String op) {
+	public Double calcular(Double n1, Double n2, String op) throws Exception {
 		double resultado = 0.0;
+		
+		if (n1 == null) {
+			n1 = 0.0;
+		}
+		
+		n2 = (n2 == null ? 0.0 : n2);
 		
 		if (op.equals("+")) {
 			resultado = n1 + n2;
 		} else if (op.equals("-")) {
 			resultado = n1 - n2;
 		} else if (op.equals("/")) {
+			if (n2 == 0.0) {
+				throw new Exception("Não é possivel dividir por zero.");
+			}
+			
 			resultado = n1 / n2;
 		} else if (op.equals("*")) {
 			resultado = n1 * n2;
